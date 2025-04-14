bd1d7b6c8c00ae9b031665fc9c64fa30f1cc8026
wookie-fatcat <nick@pinpoint.co.uk>
2025-04-14 11:25:40 +0100

    Update solisUseHistory.js
    Correct some axis labels and scale battery graph to configured Min/Max levels for clearer graphical representation of battery state.

--- mapped/www/js/assemblies/solisUseHistory.js
+++ mapped/www/js/assemblies/solisUseHistory.js
@@ -33,12 +33,19 @@ document.addEventListener('touchend', function (event) {
 });
 
         this.fetchHistory = async function(dateIndex) {
+	  let jsonbatt = await _this.context.request('/agility/config/battery');
           let json = await _this.context.request('/agility/solis/data/history/' + dateIndex);
-          if (json.error) {
+          if (jsonbatt.error) {
+            _this.toast.headerTxt = 'Error';
+            _this.toast.display(jsonbatt.error);
+          }
+	  else if (json.error) {
             _this.toast.headerTxt = 'Error';
             _this.toast.display(json.error);
           }
           else {
+	    let battMax = +jsonbatt.data.chargeLimit;
+	    let battMin = +jsonbatt.data.minimumLevel;
             let labels = [];
             let batteryLevels = [];
             let prices = [];
@@ -108,6 +115,7 @@ document.addEventListener('touchend', function (event) {
                   label: 'Grid Export',
                   data: gridExports,
                   fill: true,
+                  stepped: 'after',
                   borderColor: 'orange',
                   backgroundColor: 'yellow',
                   yAxisID: 'power'
@@ -163,15 +171,15 @@ document.addEventListener('touchend', function (event) {
                     position: 'left',
                     title: {
                       display: true,
-                      text: 'Price (ex-VAT) (p)'
+                      text: 'Price (p)'
                     }
                   },
                   battery: {
                     type: 'linear',
                     display: true,
                     position: 'left',
-                    min: 0,
-                    max: 100,
+                    min: battMin,
+                    max: battMax,
                     title: {
                       display: true,
                       text: 'Battery Level (%)'
@@ -184,7 +192,7 @@ document.addEventListener('touchend', function (event) {
                     min: 0,
                     title: {
                       display: true,
-                      text: 'Load / Grid / PV (kWh)'
+                      text: 'Load / Grid / Solar (kWh)'
                     }
                   }
                 }
